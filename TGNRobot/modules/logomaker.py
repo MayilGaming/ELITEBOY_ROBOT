6b00fc08c91ee2112151f39588c55237d82544d6
HARRY <87848754+ITX-ALONE-HARRY@users.noreply.github.com>
2021-10-12 13:30:01 +0530

    Update logomaker.py

--- TGNRobot/modules/logomaker.py
+++ TGNRobot/modules/logomaker.py
@@ -36,11 +36,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByzaid.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
+    await tbot.send_file(event.chat_id, fname2, caption="#𝙴𝙻𝙸𝚃𝙴𝙱𝙾𝚈 𝙰𝙻𝚆𝙰𝚈𝚂 𝙾𝙿")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @godfathersupport, {e}')
+   await event.reply(f'Error Report @KID_XD, {e}')
 
 
 
@@ -76,11 +76,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByEvil.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
+    await tbot.send_file(event.chat_id, fname2, caption="#𝙴𝙻𝙸𝚃𝙴𝙱𝙾𝚈 𝙰𝙻𝚆𝙰𝚈𝚂 𝙾𝙿")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Gotfathersupport, {e}')
+   await event.reply(f'Error Report @KID_XD, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
